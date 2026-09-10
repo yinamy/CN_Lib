d005e7d79860c26fe655528abbf8d22263840a4e
sandwichman3 <yinamy@outlook.com>
2026-09-10 18:35:20 +0100

    WIP support for C integer types

--- CN_Lib/CN_Lib.lean
+++ CN_Lib/CN_Lib.lean
@@ -1,9 +1,167 @@
 namespace CN_Lib
 
+-- CN's represents C integer types as mathematical integers with bounds.
+-- The following bounds are enforced by the ownership predicates defined in CN_Lib_Iris.
+-- Bounds on the unsigned integer types.
+abbrev min_UChar : Int := 0
+abbrev max_UChar : Int := 2 ^ 8 - 1
+-- TODO: what to do for Bool?
+abbrev min_UShort := 0
+abbrev max_UShort := 2 ^ 16 - 1
+abbrev min_UInt : Int := 0
+abbrev max_UInt : Int := 2 ^ 32 - 1
+abbrev min_ULong : Int := 0
+abbrev max_ULong : Int := 2 ^ 64 - 1
+
+-- Bounds on the signed integer types.
+abbrev min_Char : Int := -(2 ^ 7 - 1)
+abbrev max_Char : Int := 2 ^ 7 - 1
+abbrev min_Short : Int := -(2 ^ 15 - 1)
+abbrev max_Short : Int := 2 ^ 15 - 1
+abbrev min_Int : Int := -(2 ^ 31 - 1)
+abbrev max_Int : Int := 2 ^ 31 - 1
+abbrev min_Long : Int := -(2 ^ 63 - 1)
+abbrev max_Long : Int := 2 ^ 63 - 1
+
+--  We roll our own pseudo-bitvectors below because we still have to
+--  reason about ownership of individual Ints sometimes.
+@[simp, reducible]
+def Fin_to_Ints (n : Nat) : Int → (Fin n → Int) :=
+  fun v i => (v % 2 ^(8 * (n - i.toNat))) / (2 ^ (8 * (n - 1 - i.toNat)))
+@[simp, reducible]
+def Ints_to_Fin {n : Nat} (f : Fin n → Int) : Int :=
+  List.foldl
+    (fun acc i => acc + f i * 2 ^ (8 * (n - 1 - i.toNat)))
+    0
+    (List.finRange n)
+
+@[simp, reducible]
+def UShort_bytes (v : Int) : Fin 2 → Int :=
+  Fin_to_Ints 2 v
+
+@[simp, reducible]
+def UInt_bytes (v : Int) : Fin 4 → Int :=
+  Fin_to_Ints 4 v
+
+@[simp, reducible]
+def ULong_bytes (v : Int) : Fin 8 → Int :=
+  Fin_to_Ints 8 v
+
+@[simp, reducible]
+def bytes_UShort (f : Fin 2 → Int) : Int :=
+  Ints_to_Fin f
+
+@[simp, reducible]
+def bytes_UInt (f : Fin 4 → Int) : Int :=
+  Ints_to_Fin f
+
+@[simp, reducible]
+def bytes_ULong (f : Fin 8 → Int) : Int :=
+  Ints_to_Fin f
+
+theorem UShort_idem {v : Int} :
+   min_UShort ≤ v ∧ v ≤ max_UShort
+  → v = bytes_UShort (UShort_bytes v) :=
+by
+  intro ⟨Hv , Hv'⟩
+  simp [List.finRange]; lia
+
+theorem UInt_idem {v : Int} :
+   min_UInt ≤ v ∧ v ≤ max_UInt
+  → v = bytes_UInt (UInt_bytes v) :=
+by
+  intro ⟨Hv , Hv'⟩
+  unfold UInt_bytes Fin_to_Ints bytes_UInt Ints_to_Fin List.finRange
+  simp only [List.ofFn, Fin.foldr, Fin.foldr.loop, List.foldl]
+  simp only [Fin.toNat]
+  lia
+
+theorem ULong_idem {v : Int} :
+   min_ULong ≤ v ∧ v ≤ max_ULong
+  → v = bytes_ULong (ULong_bytes v) :=
+by
+  intro ⟨Hv , Hv'⟩
+  unfold ULong_bytes Fin_to_Ints bytes_ULong Ints_to_Fin List.finRange
+  simp only [List.ofFn, Fin.foldr, Fin.foldr.loop, List.foldl]
+  simp only [Fin.toNat]
+  lia
+
+theorem UShort_eq {v v' : Int} :
+    min_UShort ≤ v ∧ v ≤ max_UShort
+  → min_UShort ≤ v' ∧ v' ≤ max_UShort
+  → UShort_bytes v = UShort_bytes v'
+  → v = v' :=
+by
+  intros Hv Hv' H
+  unfold UShort_bytes at H
+  have HEq : v = bytes_UShort (UShort_bytes v) := by
+    apply UShort_idem; assumption
+  have HEq' : v' = bytes_UShort (UShort_bytes v') := by
+    apply UShort_idem; assumption
+  unfold UShort_bytes at HEq HEq'
+  rw [H] at HEq; symm at HEq'
+  rw [HEq'] at HEq; assumption
+
+theorem UInt_eq {v v' : Int} :
+    min_UInt ≤ v ∧ v ≤ max_UInt
+  → min_UInt ≤ v' ∧ v' ≤ max_UInt
+  → UInt_bytes v = UInt_bytes v'
+  → v = v' :=
+by
+  intros Hv Hv' H
+  unfold UInt_bytes at H
+  have HEq : v = bytes_UInt (UInt_bytes v) := by
+    apply UInt_idem; assumption
+  have HEq' : v' = bytes_UInt (UInt_bytes v') := by
+    apply UInt_idem; assumption
+  unfold UInt_bytes at HEq HEq'
+  rw [H] at HEq; symm at HEq'
+  rw [HEq'] at HEq; assumption
+
+theorem ULong_eq {v v' : Int} :
+    min_ULong ≤ v ∧ v ≤ max_ULong
+  → min_ULong ≤ v' ∧ v' ≤ max_ULong
+  → ULong_bytes v = ULong_bytes v'
+  → v = v' :=
+by
+  intros Hv Hv' H
+  unfold ULong_bytes at H
+  have HEq : v = bytes_ULong (ULong_bytes v) := by
+    apply ULong_idem; assumption
+  have HEq' : v' = bytes_ULong (ULong_bytes v') := by
+    apply ULong_idem; assumption
+  unfold ULong_bytes at HEq HEq'
+  rw [H] at HEq; symm at HEq'
+  rw [HEq'] at HEq; assumption
+
+-- Other stuff
 def wrapI (minInt : Int) (maxInt : Int) x :=
   let delta := ((maxInt - minInt) + 1)
   let r := x % delta
   (if (r <= maxInt) then r else r - delta)
+
+theorem wrapI_within_bounds :
+  ∀ {min max x : Int},
+  (min ≤ 0 ∧ 0 < max) →
+  min ≤ wrapI min max x ∧ wrapI min max x ≤ max :=
+by
+  intro min max x H
+  unfold wrapI
+  let delta := max - min + 1
+  simp; constructor
+  · by_cases h : x % (max - min + 1) ≤ max
+    · simp [h]
+      have hnonneg : 0 ≤ x % (max - min + 1) := by
+        exact Int.emod_nonneg x (by omega)
+      omega
+    · simp [h]; omega
+  · by_cases h : x % (max - min + 1) ≤ max
+    · simp [h]
+    · simp [h]
+      have hd : 0 < max - min + 1 := by omega
+      have hmod : x % (max - min + 1) < max - min + 1 := by
+        exact Int.emod_lt_of_pos x hd
+      omega
 
 theorem dvd_mod_minus (a b c : Int):
   (0 ≤ c) ∧ (c < b) → (b ∣ a - c) → a % b = c :=
@@ -13,22 +171,140 @@ by
   simp [ha, Int.add_mul_emod_self_left, Int.emod_eq_of_lt hc0 hcb]
 
 theorem wrapI_idem:
-  ∀ (minInt maxInt x : Int),
-  (minInt ≤ x ∧ x ≤ maxInt) →
-  (minInt ≤ 0 ∧ 0 < maxInt) →
-  wrapI minInt maxInt x = x :=
+  ∀ {min max x : Int},
+  (min ≤ x ∧ x ≤ max) →
+  (min ≤ 0 ∧ 0 < max) →
+  wrapI min max x = x :=
 by
-  intros minInt maxInt x H1 H2
+  intros min max x H1 H2
   unfold wrapI
-  let delta := ((maxInt - minInt) + 1)
+  let delta := ((max - min) + 1)
   by_cases hx : 0 ≤ x
   · simp
     rw [Int.emod_eq_of_lt]
     omega; exact hx ;omega
   · simp
     rw [dvd_mod_minus _ _ (x + delta)]
-    · by_cases hx' : (x + delta <= maxInt)
+    · by_cases hx' : (x + delta <= max)
       · omega
       · omega
     · omega
     · exists (-1); omega
+
+theorem wrapI_pos_neg_neq :
+  ∀ {max Umax v v' : Int} {_: Umax = 2 * max + 1},
+  0 ≤ v ∧ v ≤ max →
+  -max ≤ v' ∧ v' < 0 →
+  wrapI 0 Umax v ≠ wrapI 0 Umax v' :=
+by
+  intros max Umax v v' inv Hv Hv'
+  unfold wrapI
+  simp [inv, Int.add_assoc]
+  have hvrem : v % (2 * max + 2) = v := by
+    apply Int.emod_eq_of_lt
+    · exact ‹0 ≤ v ∧ v ≤ max›.1
+    · omega
+  have hv'rem : v' % (2 * max + 2) = v' + (2 * max + 2) := by
+    rw [Int.emod_eq_add_self_emod]
+    apply Int.emod_eq_of_lt; omega; omega
+  rw [hvrem, hv'rem]; omega
+
+theorem wrapI_cong_pos :
+  ∀ {max Umax v v' : Int} {_ : 0 ≤ max}
+    {_ : Umax = (2 * max + 1)},
+    0 ≤ v ∧ v ≤ max
+  → 0 ≤ v' ∧ v' ≤ max
+  → wrapI 0 Umax v = wrapI 0 Umax v'
+  → v = v' :=
+by
+  intro max Umax v v' hpos inv ⟨H1, H1'⟩ ⟨H2, H2'⟩ Hwrap
+  have HEq : wrapI 0 Umax v = v := by
+        apply wrapI_idem; lia
+        simp [inv]; lia
+  have HEq' : wrapI 0 Umax v' = v' := by
+    apply wrapI_idem; lia
+    simp [inv]; lia
+  rw [HEq, HEq'] at Hwrap; assumption
+
+theorem wrapI_cong_neg :
+  ∀ {max Umax v v' : Int} {_ : 0 ≤ max}
+    {_ : Umax = (2 * max + 1)},
+    -max ≤ v ∧ v < 0
+  → -max ≤ v' ∧ v' < 0
+  → wrapI 0 Umax v = wrapI 0 Umax v'
+  → v = v' :=
+by
+  intro max Umax v v' hmax hU hv hv' hEq
+  have hmod : 0 < 2 * max + 2 := by omega
+  have hrem : v % (2 * max + 2) < 2 * max + 2 := by
+    exact Int.emod_lt_of_pos v hmod
+  have hrem' : v' % (2 * max + 2) < 2 * max + 2 := by
+    exact Int.emod_lt_of_pos v' hmod
+  have h : v % (2 * max + 2) ≤ 2 * max + 1 := by omega
+  have h' : v' % (2 * max + 2) ≤ 2 * max + 1 := by omega
+  simp [wrapI, hU, Int.add_assoc, h, h'] at hEq
+  have hvrem :
+    v % (2 * max + 2) = v + (2 * max + 2) := by
+    rw [Int.emod_eq_add_self_emod]
+    apply Int.emod_eq_of_lt <;> omega
+  have hvrem' :
+    v' % (2 * max + 2) = v' + (2 * max + 2) := by
+    rw [Int.emod_eq_add_self_emod]
+    apply Int.emod_eq_of_lt <;> omega
+  rw [hvrem, hvrem'] at hEq
+  omega
+
+theorem wrapI_cong :
+  ∀ {max v v' : Int}, 0 ≤ max
+  → -max ≤ v ∧ v ≤ max
+  → -max ≤ v' ∧ v' ≤ max
+  → wrapI 0 (2 * max + 1) v = wrapI 0 (2 * max + 1) v'
+  → v = v' :=
+by
+  intro max v v' hpos ⟨H1, H1'⟩ ⟨H2, H2'⟩ Hwrap
+  by_cases h0 : 0 ≤ v
+  · by_cases h0' : 0 ≤ v'
+    · have h : 0 ≤ v ∧ v ≤ max := by omega
+      have h' : 0 ≤ v' ∧ v' ≤ max := by omega
+      apply wrapI_cong_pos h h' Hwrap;
+      assumption; rfl
+    · exfalso
+      have h : 0 ≤ v ∧ v ≤ max := by omega
+      have h' : -max ≤ v' ∧ v' < 0 := by omega
+      apply wrapI_pos_neg_neq h h' Hwrap; rfl
+  · by_cases h0' : 0 ≤ v'
+    · exfalso
+      have h : -max ≤ v ∧ v < 0 := by omega
+      have h' : 0 ≤ v' ∧ v' ≤ max := by omega
+      apply wrapI_pos_neg_neq h' h Hwrap.symm; rfl
+    · have h : -max ≤ v ∧ v < 0 := by omega
+      have h' : -max ≤ v' ∧ v' < 0 := by omega
+      apply wrapI_cong_neg h h' Hwrap;
+      assumption; rfl
+
+theorem wrapI_Char_cong :
+  min_Char ≤ v ∧ v ≤ max_Char
+  → min_Char ≤ v' ∧ v' ≤ max_Char
+  → wrapI min_UChar max_UChar v = wrapI min_UChar max_UChar v'
+  → v = v' :=
+by
+  intro Hv Hv' Hwrap
+  apply wrapI_cong _ Hv Hv' Hwrap; omega
+
+theorem wrapI_Short_cong :
+  min_Short ≤ v ∧ v ≤ max_Short
+  → min_Short ≤ v' ∧ v' ≤ max_Short
+  → wrapI min_UShort max_UShort v = wrapI min_UShort max_UShort v'
+  → v = v' :=
+by
+  intro Hv Hv' Hwrap
+  apply wrapI_cong _ Hv Hv' Hwrap; omega
+
+theorem wrapI_Int_cong :
+  min_Int ≤ v ∧ v ≤ max_Int
+  → min_Int ≤ v' ∧ v' ≤ max_Int
+  → wrapI min_UInt max_UInt v = wrapI min_UInt max_UInt v'
+  → v = v' :=
+by
+  intro Hv Hv' Hwrap
+  apply wrapI_cong _ Hv Hv' Hwrap; omega
--- CN_Lib/CN_Lib_Iris.lean
+++ CN_Lib/CN_Lib_Iris.lean
@@ -5,15 +5,19 @@ import Iris.BI.Lib.GenHeap
 import Iris.Std.GenSetsInstances
 import Iris.ProofMode
 import Std.Data.ExtTreeMap
+import CN_Lib.CN_Lib
+import Lean.Elab
+import Lean
 
-open Iris ProgramLogic Language.Notation Std FromMathlib
+open Lean Elab Tactic Qq
+open Iris ProgramLogic Language.Notation Std FromMathlib BI
 
 namespace CN_Lib
 
 -- Library for things needed by CN lemma exports.
 
 -- 1, Resource algebra for heaps that are maps from integers
---  to integers. Nothing fancy and copied from HeapLang.
+--  to integers.
 
 abbrev myHeapF := fun V => Std.ExtTreeMap Int V compare
 
@@ -32,98 +36,200 @@ class MyHeap (hlc : outParam HasLC) (GF : BundledGFunctors) extends genHeapGS Pt
 variable [MyHeap hlc GF]
 
 
--- 2, Now we add basic CN-style ownership predicates.
+-- 2, CN-style ownership predicates.
+-- Every ownership predicate reduces to ownership of unsigned
+-- bytes and a proof that the value is within bounds for that type.
 
--- Placeholder for int to bytes conversion
-abbrev byte := Int × Int × Int × Int
-def int_to_bytes (v : Int) : byte := sorry
+-- Ownership of unsigned C integer types
+def Owned_UChar (l : Ptr) (v : Int) : IProp GF := iprop%
+    (l ↦ some v) -- Pointsto predicate
+  ∗ ⌜l ≠ 0⌝ -- Pointer is non-null
+  ∗ ⌜ min_UChar ≤ v ∧ v ≤ max_UChar ⌝ -- Value is within bounds
+def Owned_UShort (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := UShort_bytes v
+    iprop% Owned_UChar l (bytes 0)
+  ∗ Owned_UChar (l + 1) (bytes 1)
+  ∗ ⌜ min_UShort ≤ v ∧ v ≤ max_UShort ⌝
+def Owned_UInt (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := UInt_bytes v
+  iprop%
+    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
+  ∗ ⌜ min_UInt ≤ v ∧ v ≤ max_UInt ⌝
+def Owned_ULong (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := ULong_bytes v
+  iprop%
+    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
+  ∗ ⌜ min_ULong ≤ v ∧ v ≤ max_ULong ⌝
 
--- Generic ownership
-def Owned (l : Ptr) (v : Int) : IProp GF := iprop% (l ↦ some v) ∧ ⌜l ≠ 0⌝
-def Block (l : Ptr) : IProp GF := iprop% (l ↦ none) ∧ ⌜l ≠ 0⌝
+-- Ownership of signed C integer types
+def Owned_Char (l : Ptr) (v : Int) : IProp GF :=
+  let byte := wrapI min_UChar max_UChar v
+  iprop%
+    Owned_UChar l byte
+  ∗ ⌜ min_Char ≤ v ∧ v ≤ max_Char ⌝
+def Owned_Short (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := UShort_bytes (wrapI min_UShort max_UShort v)
+  iprop%
+    Owned_UChar l (bytes 0)
+  ∗ Owned_UChar (l + 1) (bytes 1)
+  ∗ ⌜ min_Short ≤ v ∧ v ≤ max_Short ⌝
+def Owned_Int (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := UInt_bytes (wrapI min_UInt max_UInt v)
+  iprop%
+    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
+  ∗ ⌜ min_Int ≤ v ∧ v ≤ max_Int ⌝
+def Owned_Long (l : Ptr) (v : Int) : IProp GF :=
+  let bytes := ULong_bytes (wrapI min_ULong max_ULong v)
+  iprop%
+    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
+  ∗ ⌜ min_Long ≤ v ∧ v ≤ max_Long ⌝
 
--- Ownership of C types
-def Owned_char (l : Ptr) (v : Int) : IProp GF := Owned l v
-def Owned_int (l : Ptr) (v : Int) : IProp GF :=
-  let (b1, b2, b3, b4) := int_to_bytes v
-    iprop% Owned l b1
-    ∗ Owned (l + 1) b2
-    ∗ Owned (l + 2) b3
-    ∗ Owned (l + 3) b4
--- TODO: more C types, e.g. struct, array, etc.
 
 -- 3, Array ownership
 
--- Arrayshift
+def Block (l : Ptr) : IProp GF := iprop% (l ↦ none) ∧ ⌜l ≠ 0⌝
+
 def arrayshift (l : Ptr) (pos : Int) (size : Int) : Ptr := l + pos * size
+
 def padding (l : Ptr) (n : Nat) : IProp GF :=
   match n with
   | 0 => l ↦ none
   | Nat.succ n' => iprop% (Block l ∗ padding (l + 1) n')
 
 
--- 4, Useful lemmas
-
--- Owned pointers can't be null
+-- 4, Pointer non-null theorems
 theorem ptr_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned l v → ⌜l ≠ 0⌝ :=
+  ⊢@{IProp GF} Owned_UChar l v → ⌜l ≠ 0⌝ :=
 by
-  unfold Owned
-  iintro ⟨_ , HOwn⟩; iframe
+  unfold Owned_UChar
+  iintro ⟨_ , ⟨HOwn, _⟩⟩; iframe
 
 -- Owned integer pointers can't be null
 theorem ptr_int_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_int l v → ⌜l ≠ 0⌝ :=
+  ⊢@{IProp GF} Owned_Int l v → ⌜l ≠ 0⌝ :=
 by
-  unfold Owned_int Owned
-  iintro ⟨⟨ _ , H⟩ , _⟩; iframe
+  unfold Owned_Int Owned_UChar
+  iintro ⟨H , _⟩
+  simp
+  icases H with ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
 
--- Two owned pointers must be different
-theorem owned_neq {l l' : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned l v -∗ Owned l' v' -∗ ⌜l ≠ l'⌝ :=
-by
-  unfold Owned
-  iintro ⟨H1 , _⟩ ⟨H2 , _⟩
-  iapply pointsTo_ne $$ H1 H2
+-- TODO: fill these in
 
--- Two owned char pointers must be different
-theorem owned_char_neq {l l' : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_char l v -∗ Owned_char l' v' -∗ ⌜l ≠ l'⌝ :=
-by
-  unfold Owned_char
-  iintro H1 H2
-  iapply owned_neq $$ H1 H2
+-- 5, Equality theorems for ownership predicates
 
--- Two owned int pointers must be different
-theorem owned_int_neq {l l' : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_int l v -∗ Owned_int l' v' -∗ ⌜l ≠ l'⌝ :=
+theorem Owned_UChar_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_UChar l v -∗ Owned_UChar l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_int; simp
-  iintro ⟨H1 , _⟩ ⟨H2 , _⟩
-  iapply owned_neq $$ H1 H2
-
--- Two copies of ownership of the same pointer must have the same value
-theorem owned_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned l v -∗ Owned l v' -∗ ⌜v = v'⌝ :=
-by
-  unfold Owned
-  iintro ⟨H1 , _⟩ ⟨H2 , _⟩
+  unfold Owned_UChar min_UChar max_UChar
+  iintro ⟨H1 , ⟨_ , ⟨%h1, %h1'⟩⟩⟩ ⟨H2 , ⟨_ , ⟨%h2 , %h2'⟩⟩⟩
   ihave %H := pointsTo_agree $$ [$]
-  ipureintro; injection H; omega
+  ipureintro; injection H with val_eq; lia
 
--- Two copies of ownership of the same char pointer must have the same value
-theorem owned_char_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_char l v -∗ Owned_char l v' -∗ ⌜v = v'⌝ :=
+theorem Owned_Char_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Char l v -∗ Owned_Char l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_char
-  iintro H1 H2
-  iapply owned_eq $$ H1 H2
+  unfold Owned_Char min_Char max_Char
+  iintro ⟨H1 , %H2⟩ ⟨H3 , %H4⟩
+  ihave %HEq := Owned_UChar_eq $$ H1 H3
+  ipureintro
+  apply wrapI_Char_cong H2 H4 HEq
 
--- Two copies of ownership of the same int pointer must have the same value
-theorem owned_int_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_int l v -∗ Owned_int l v' -∗ ⌜v = v'⌝ :=
+theorem Owned_UShort_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_UShort l v -∗ Owned_UShort l v' -∗ ⌜v = v'⌝ :=
 by
-  sorry
--- TODO: Omitted until int_to_bytes is implemented
+  unfold Owned_UShort min_UShort max_UShort
+  iintro ⟨H1 , ⟨H2 , ⟨%H⟩⟩⟩ ⟨H5 , ⟨H6 , ⟨%H'⟩⟩⟩
+  ihave %HEq := Owned_UChar_eq $$ H1 H5
+  ihave %HEq' := Owned_UChar_eq $$ H2 H6
+  ipureintro
+  have Hbytes : UShort_bytes v = UShort_bytes v' := by
+    funext i
+    repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
+    apply Fin.elim0; assumption
+  have H := UShort_eq H H' Hbytes
+  assumption
 
-end CN_Lib
+theorem Owned_Short_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Short l v -∗ Owned_Short l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_Short
+  iintro ⟨H1 , ⟨H2 , %H⟩⟩ ⟨H3 , ⟨H4 , %H'⟩⟩
+  ihave %HEq := Owned_UChar_eq $$ H1 H3
+  ihave %HEq' := Owned_UChar_eq $$ H2 H4
+  ipureintro
+  have Hbytes : UShort_bytes (wrapI min_UShort max_UShort v) = UShort_bytes (wrapI min_UShort max_UShort v') := by
+    funext i
+    repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
+    apply Fin.elim0; assumption
+  have Hv := wrapI_within_bounds (x := v) (min := min_UShort) (max := max_UShort) (by lia)
+  have Hv' := wrapI_within_bounds (x := v') (min := min_UShort) (max := max_UShort) (by lia)
+  have H1 := UShort_eq Hv Hv' Hbytes
+  apply wrapI_Short_cong H H' H1
+
+theorem Owned_UInt_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_UInt l v -∗ Owned_UInt l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_UInt
+  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
+  simp [-UInt_bytes]
+  icases HOwn with ⟨H1 , ⟨H2 , ⟨H3 , ⟨H4 , _⟩⟩⟩⟩
+  icases HOwn' with ⟨H1' , ⟨H2' , ⟨H3' , ⟨H4' , _⟩⟩⟩⟩
+  ihave %H1 := Owned_UChar_eq $$ H1 H1'
+  ihave %H2 := Owned_UChar_eq $$ H2 H2'
+  ihave %H3 := Owned_UChar_eq $$ H3 H3'
+  ihave %H4 := Owned_UChar_eq $$ H4 H4'
+  ipureintro
+  have Hbytes : UInt_bytes v = UInt_bytes v' := by
+    funext i
+    repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
+    apply Fin.elim0; assumption
+  have H1 := UInt_eq H H' Hbytes
+  assumption
+
+theorem Owned_Int_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Int l v -∗ Owned_Int l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_Int
+  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
+  simp [-UInt_bytes]
+  icases HOwn with ⟨H1 , ⟨H2 , ⟨H3 , ⟨H4 , _⟩⟩⟩⟩
+  icases HOwn' with ⟨H1' , ⟨H2' , ⟨H3' , ⟨H4' , _⟩⟩⟩⟩
+  ihave %H1 := Owned_UChar_eq $$ H1 H1'
+  ihave %H2 := Owned_UChar_eq $$ H2 H2'
+  ihave %H3 := Owned_UChar_eq $$ H3 H3'
+  ihave %H4 := Owned_UChar_eq $$ H4 H4'
+  ipureintro
+  have Hbytes : UInt_bytes (wrapI min_UInt max_UInt v) = UInt_bytes (wrapI min_UInt max_UInt v') := by
+    funext i
+    repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
+    apply Fin.elim0; assumption
+  have Hv := wrapI_within_bounds (x := v) (min := min_UInt) (max := max_UInt) (by lia)
+  have Hv' := wrapI_within_bounds (x := v') (min := min_UInt) (max := max_UInt) (by lia)
+  have H1 := UInt_eq Hv Hv' Hbytes
+  apply wrapI_Int_cong H H' H1
+
+theorem Owned_ULong_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_ULong l v -∗ Owned_ULong l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_ULong
+  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
+  simp [-ULong_bytes]
+  icases HOwn with ⟨H1 , ⟨H2 , ⟨H3 , ⟨H4 , HOwn⟩⟩⟩⟩
+  icases HOwn with ⟨H5 , ⟨H6 , ⟨H7 , ⟨H8 , _⟩⟩⟩⟩
+  icases HOwn' with ⟨H1' , ⟨H2' , ⟨H3' , ⟨H4' , HOwn'⟩⟩⟩⟩
+  icases HOwn' with ⟨H5' , ⟨H6' , ⟨H7' , ⟨H8' , _⟩⟩⟩⟩
+  ihave %H1 := Owned_UChar_eq $$ H1 H1'
+  ihave %H2 := Owned_UChar_eq $$ H2 H2'
+  ihave %H3 := Owned_UChar_eq $$ H3 H3'
+  ihave %H4 := Owned_UChar_eq $$ H4 H4'
+  ihave %H5 := Owned_UChar_eq $$ H5 H5'
+  ihave %H6 := Owned_UChar_eq $$ H6 H6'
+  ihave %H7 := Owned_UChar_eq $$ H7 H7'
+  ihave %H8 := Owned_UChar_eq $$ H8 H8'
+  ipureintro
+  have Hbytes : ULong_bytes v = ULong_bytes v' := by
+    funext i
+    repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
+    apply Fin.elim0; assumption
+  have H1 := ULong_eq H H' Hbytes
+  assumption
