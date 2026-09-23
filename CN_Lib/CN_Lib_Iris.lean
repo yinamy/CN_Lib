f19cd76ab353184f27e6338efb3932aba7db22b3
sandwichman3 <yinamy@outlook.com>
2026-09-23 16:07:51 +0100

    Added VIP-style resource algebra

--- CN_Lib/CN_Lib_Iris.lean
+++ CN_Lib/CN_Lib_Iris.lean
@@ -8,297 +8,293 @@ import Std.Data.ExtTreeMap
 import CN_Lib.CN_Lib
 import CN_Lib.CN_Lib_Tactics
 
-open Iris ProgramLogic Language.Notation Std FromMathlib BI CN_Lib
+open Iris ProgramLogic Language.Notation Std FromMathlib BI
 
 namespace CN_Lib
 
 -- Library for things needed by CN lemma exports.
 
--- 1, Resource algebra for heaps that are maps from integers
---  to integers.
+-- 1, Resource algebra for heaps that are maps from integers to integers.
 
-abbrev myHeapF := fun V => Std.ExtTreeMap Int V compare
+abbrev Addr    := Int -- Addresses are integers
+abbrev AllocId := Int -- Allocation IDs are integers
 
--- Pointers are just integers for now
-abbrev Ptr := Int
-instance : OfNat Ptr n := ⟨n⟩
--- Values are option integers for now
-abbrev Val := Option Int
+structure Loc where -- Locations are address/provenance pairs
+  addr         : Addr
+  id           : AllocId
 
--- class MyHeap (hlc : outParam HasLC) (GF : BundledGFunctors) where
---   heap : genHeapGS Ptr Val GF myHeapF
+abbrev Ptr     := Option Loc -- Pointers are either null or a location
+abbrev Val     := Option Int -- Values are option integers for now
 
-class MyHeap (hlc : outParam HasLC) (GF : BundledGFunctors) extends genHeapGS Ptr Val GF myHeapF
+/- The allocation history is a map from allocation IDs to the base address and the
+  size of the allocation. Let's call the (base address, size) pair "AllocMetaData". -/
+structure AllocMetaData where
+  base         : Addr
+  size          : Nat
 
--- attribute [reducible, instance] MyHeap.heap
+abbrev Alloc_HistoryF := fun V => Std.ExtTreeMap AllocId V compare
 
-variable [MyHeap hlc GF]
+-- The liveset is a set of allocation IDs
+abbrev LiveSet := Auth (LeibnizSet (Std.ExtTreeSet AllocId compare))
+abbrev LiveSetF := constOF LiveSet
 
+/- We stagger the definition of the allocation history resource algebra into two
+  classes. The first class defines the allocation history resource algebra. -/
+class allocHistPreS (GF : BundledGFunctors) where
+  allocmap : GhostMapG GF AllocId AllocMetaData Alloc_HistoryF
+  liveset  : ElemG GF LiveSetF
+attribute [reducible, instance] allocHistPreS.allocmap
+attribute [reducible, instance] allocHistPreS.liveset
+
+/- The second class ensures only one instane of allocHistPreS existsm by fixing
+  two ghost names for the ghost map and the liveset. -/
+class allocHistGS (GF : BundledGFunctors) extends allocHistPreS GF where
+  allocmap_name : GName
+  liveset_name  : GName
+
+-- The heap is a map from addresses to values
+abbrev VIP_HeapF := fun V => Std.ExtTreeMap Addr V compare
+
+-- The CN-VIP heap is a tuple ((A, L), M) = ((allocation history, liveset), memory)
+class VIP_HeapGS (GF : BundledGFunctors) where
+  provenance : allocHistGS GF
+  memory     : genHeapGS Addr Val GF VIP_HeapF
+
+attribute [reducible, instance] VIP_HeapGS.provenance
+attribute [reducible, instance] VIP_HeapGS.memory
+
+variable {GF : BundledGFunctors} [G : VIP_HeapGS GF]
+
+-- Here is some nicer syntax for looking stuff up in the allocation history.
+def AllocHist_elem (l : AllocId) (v : AllocMetaData) : IProp GF :=
+  (allocHistGS.allocmap_name GF) ↪◯MAP[l] v
+
+def Live (l : AllocId) : IProp GF :=
+  iOwn (E := allocHistPreS.liveset) (allocHistGS.liveset_name GF) (◯ (.valid { l }))
+
+syntax "AllocHistory[@" term "]" "=>" "(" term ("," term)? ")" : term
+macro_rules
+  | `(AllocHistory[@$l] => ($v , true))  => `(iprop% AllocHist_elem $l $v ∗ Live $l)
+  | `(AllocHistory[@$l] => ($v , false))  => `(iprop% AllocHist_elem $l $v ∗ ¬ Live $l)
+  | `(AllocHistory[@$l] => ($v))  => `(iprop% AllocHist_elem $l $v)
+
+-- Example usage of new syntax
+def alloc_entry_example (l : AllocId) (b : Addr) (n : Nat) : IProp GF :=
+  AllocHistory[@l] => ({ base := b, size := n }, true)
+-- The following is also valid and corresponds to `A[@l] = { (b, n) , _ }`
+def alloc_entry_example2 (l : AllocId) (b : Addr) (n : Nat) : IProp GF :=
+  AllocHistory[@l] => ({ base := b, size := n })
 
 -- 2, CN-style ownership predicates.
---  Every ownership predicate reduces to ownership of unsigned
---  bytes and a proof that the value is within bounds for that type.
+--  Every ownership predicate reduces to the generic 'Owned' predicate:
+def Owned (ptr : Ptr) (val : Int)
+          (min max : Int)
+          (n : Nat) (bytes : Fin n → Int) : IProp GF := iprop%
+  -- Assertions about the heap
+    ∃ (l : Loc), ⌜ ptr = some l ⌝          -- Pointer is non-null
+  ∗ ([∗list] i ↦ v ∈ List.ofFn bytes,
+      (l.addr + i) ↦ (some v))             -- Points-to for each byte
+  ∗ ⌜ min ≤ val ∧ val ≤ max ⌝              -- Value is within bounds
+  -- Assertions about pointer provenance
+  ∗ ∃ (b : Addr) (n' : Nat),               -- Allocation history entry exists
+      AllocHistory[@l.id] => ({ base := b, size := n' })
+  ∗ ⌜ b ≤ l.addr ∧ l.addr ≤ b + n ⌝        -- Address is within bounds
 
--- Unsigned C integer types
-def Owned_UChar (l : Ptr) (v : Int) : IProp GF := iprop%
-    (l ↦ some v) -- Pointsto predicate
-  ∗ ⌜l ≠ 0⌝ -- Pointer is non-null
-  ∗ ⌜ min_UChar ≤ v ∧ v ≤ max_UChar ⌝ -- Value is within bounds
-def Owned_UShort (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := UShort_bytes v
-    iprop%
-    (Owned_UChar l (bytes 0) ∗ Owned_UChar (l + 1) (bytes 1))
-  ∗ ⌜ min_UShort ≤ v ∧ v ≤ max_UShort ⌝
-def Owned_UInt (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := UInt_bytes v
-  iprop%
-    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
-  ∗ ⌜ min_UInt ≤ v ∧ v ≤ max_UInt ⌝
-def Owned_ULong (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := ULong_bytes v
-  iprop%
-    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
-  ∗ ⌜ min_ULong ≤ v ∧ v ≤ max_ULong ⌝
+-- Ownership of unsigned integer types
+@[simp, reducible]
+def Owned_UChar (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_UChar max_UChar 1 (fun _ => val)
+@[simp, reducible]
+def Owned_UShort (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_UShort max_UShort 2 (UShort_bytes val)
+@[simp, reducible]
+def Owned_UInt (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_UInt max_UInt 4 (UInt_bytes val)
+@[simp, reducible]
+def Owned_ULong (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_ULong max_ULong 8 (ULong_bytes val)
 
--- Signed C integer types
-def Owned_Char (l : Ptr) (v : Int) : IProp GF :=
-  let byte := wrapI min_UChar max_UChar v
-  iprop%
-    Owned_UChar l byte
-  ∗ ⌜ min_Char ≤ v ∧ v ≤ max_Char ⌝
-def Owned_Short (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := UShort_bytes (wrapI min_UShort max_UShort v)
-  iprop%
-    (Owned_UChar l (bytes 0) ∗ Owned_UChar (l + 1) (bytes 1))
-  ∗ ⌜ min_Short ≤ v ∧ v ≤ max_Short ⌝
-def Owned_Int (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := UInt_bytes (wrapI min_UInt max_UInt v)
-  iprop%
-    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
-  ∗ ⌜ min_Int ≤ v ∧ v ≤ max_Int ⌝
-def Owned_Long (l : Ptr) (v : Int) : IProp GF :=
-  let bytes := ULong_bytes (wrapI min_ULong max_ULong v)
-  iprop%
-    ([∗list] i ↦ b ∈ List.ofFn bytes, Owned_UChar (l + i) b)
-  ∗ ⌜ min_Long ≤ v ∧ v ≤ max_Long ⌝
+-- Ownership of signed integer types
+@[simp, reducible]
+def Owned_Char (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_Char max_Char 1 (fun _ => wrapI min_UChar max_UChar val)
+@[simp, reducible]
+def Owned_Short (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_Short max_Short 2
+    (UShort_bytes (wrapI min_UShort max_UShort val))
+@[simp, reducible]
+def Owned_Int (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_Int max_Int 4
+    (UInt_bytes (wrapI min_UInt max_UInt val))
+@[simp, reducible]
+def Owned_Long (ptr : Ptr) (val : Int) : IProp GF := iprop%
+  Owned ptr val min_Long max_Long 8
+    (ULong_bytes (wrapI min_ULong max_ULong val))
 
 -- 3, Array ownership
+
+def Block (ptr : Ptr) (L : List Int) : IProp GF := iprop%
+  -- Assertions about the heap
+    ∃ (l : Loc), ⌜ ptr = some l ⌝         -- Pointer is non-null
+  ∗ ([∗list] i ∈ L, (l.addr + i) ↦ none)  -- Points-to for each byte
+  -- Assertions about pointer provenance
+  ∗ ∃ (b : Addr) (n' : Nat),              -- Allocation history entry exists
+      AllocHistory[@l.id] => ({ base := b, size := n' })
+  ∗ ⌜ b ≤ l.addr ∧ l.addr ≤ b + n' ⌝      -- Address is within bounds
+
+-- Ownership of integer type-sized blocks
 def Block_Char (l : Ptr) : IProp GF := iprop%
-  (l ↦ none) ∧ ⌜l ≠ 0⌝
+  Block l [0]
 def Block_Short (l : Ptr) : IProp GF := iprop%
-  Block_Char l ∗ Block_Char (l + 1)
+  Block l [0, 1]
 def Block_Int (l : Ptr) : IProp GF := iprop%
-  ([∗list] i ∈ [0, 1, 2, 3], Block_Char (l + i))
+  Block l [0, 1, 2, 3]
 def Block_Long (l : Ptr) : IProp GF := iprop%
-  ([∗list] i ∈ [0, 1, 2, 3, 4, 5, 6, 7], Block_Char (l + i))
+  Block l [0, 1, 2, 3, 4, 5, 6, 7]
 
--- 4, Pointer non-null theorems for Owned and Block
-theorem Owned_UChar_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_UChar l v → ⌜l ≠ 0⌝ :=
+-- 4, Value equality theorems for Owned
+-- Helper theorem 1
+theorem points_agree_addr_1 {l : Loc} {v v' : Int} :
+  ⊢@{IProp GF} ((l.addr) ↦ some v) -∗ ((l.addr) ↦ some v') -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_UChar
-  iintro ⟨_ , ⟨HOwn, _⟩⟩; iframe
-
-theorem Owned_Char_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_Char l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_Char Owned_UChar
-  iintro ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
-
-theorem Owned_UShort_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_UShort l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_UShort Owned_UChar
-  iintro ⟨⟨⟨_ , ⟨H , _⟩⟩ , _⟩ , _⟩; iframe
-
-theorem Owned_Short_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_Short l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_Short Owned_UChar
-  iintro ⟨⟨⟨_ , ⟨H , _⟩⟩ , _⟩ , _⟩; iframe
-
-theorem Owned_UInt_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_UInt l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_UInt Owned_UChar
-  iintro ⟨H , _⟩; simp
-  icases H with ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
-
-theorem Owned_Int_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_Int l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_Int Owned_UChar
-  iintro ⟨H , _⟩; simp
-  icases H with ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
-
-theorem Owned_ULong_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_ULong l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_ULong Owned_UChar
-  iintro ⟨H , _⟩; simp
-  icases H with ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
-
-theorem Owned_Long_nonnull {l : Ptr} {v : Int} :
-  ⊢@{IProp GF} Owned_Long l v → ⌜l ≠ 0⌝ :=
-by
-  unfold Owned_Long Owned_UChar
-  iintro ⟨H , _⟩; simp
-  icases H with ⟨⟨_ , ⟨H , _⟩⟩ , _⟩; iframe
-
-theorem Block_Char_nonnull {l : Ptr} :
-  ⊢@{IProp GF} Block_Char l → ⌜l ≠ 0⌝ :=
-by
-  unfold Block_Char
-  iintro ⟨_ , _⟩; iframe
-
-theorem Block_Short_nonnull {l : Ptr} :
-  ⊢@{IProp GF} Block_Short l → ⌜l ≠ 0⌝ :=
-by
-  unfold Block_Short
-  iintro ⟨_ , _⟩; iapply Block_Char_nonnull; iframe
-
-theorem Block_Int_nonnull {l : Ptr} :
-  ⊢@{IProp GF} Block_Int l → ⌜l ≠ 0⌝ :=
-by
-  unfold Block_Int; simp
-  iintro ⟨_ , _⟩; iapply Block_Char_nonnull; iframe
-
-theorem Block_Long_nonnull {l : Ptr} :
-  ⊢@{IProp GF} Block_Long l → ⌜l ≠ 0⌝ :=
-by
-  unfold Block_Long; simp
-  iintro ⟨_ , _⟩; iapply Block_Char_nonnull; iframe
-
--- 5, Value equality theorems for Owned
-theorem Owned_UChar_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_UChar l v -∗ Owned_UChar l v' -∗ ⌜v = v'⌝ :=
-by
-  unfold Owned_UChar min_UChar max_UChar
-  iintro ⟨H1 , ⟨_ , ⟨%h1, %h1'⟩⟩⟩ ⟨H2 , ⟨_ , ⟨%h2 , %h2'⟩⟩⟩
+  iintro H1 H2
   ihave %H := pointsTo_agree $$ [$]
   ipureintro; injection H with val_eq; lia
 
-theorem Owned_Char_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_Char l v -∗ Owned_Char l v' -∗ ⌜v = v'⌝ :=
+-- Helper theorem 2
+theorem points_agree_addr_i {l : Loc} {i : Int} {v v' : Int} :
+  ⊢@{IProp GF} ((l.addr + i) ↦ some v) -∗ ((l.addr + i) ↦ some v') -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_Char min_Char max_Char
-  iintro ⟨H1 , %H2⟩ ⟨H3 , %H4⟩
-  ihave %HEq := Owned_UChar_eq $$ H1 H3
-  ipureintro; apply wrapI_Char_cong H2 H4 HEq
+  iintro H1 H2
+  ihave %H := pointsTo_agree $$ [$]
+  ipureintro; injection H with val_eq; lia
+
+theorem Owned_UChar_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_UChar l v -∗ Owned_UChar l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_UChar Owned
+  iintro ⟨%x , ⟨%HEq , ⟨HOwn , _⟩⟩⟩ ⟨%x' , ⟨%HEq' , ⟨HOwn' , _⟩⟩⟩
+  simp
+  icases HOwn with ⟨H1 , _⟩; icases HOwn' with ⟨H2 , _⟩
+  have HEq : x' = x := by simpa [HEq'] using HEq
+  rw [HEq]
+  ihave %H := pointsTo_agree $$ [$]
+  injection H with val_eq
+  ipureintro; lia
 
 theorem Owned_UShort_eq {l : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_UShort l v -∗ Owned_UShort l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_UShort
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
-
-theorem Owned_Short_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_Short l v -∗ Owned_Short l v' -∗ ⌜v = v'⌝ :=
-by
-  unfold Owned_Short
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
+  unfold Owned_UShort Owned
+  solve_owned_eq_unsigned UShort_bytes
+    points_agree_addr_1 points_agree_addr_i v v'
 
 theorem Owned_UInt_eq {l : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_UInt l v -∗ Owned_UInt l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_UInt
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  simp [-UInt_bytes]
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
-
-theorem Owned_Int_eq {l : Ptr} {v v' : Int} :
-  ⊢@{IProp GF} Owned_Int l v -∗ Owned_Int l v' -∗ ⌜v = v'⌝ :=
-by
-  unfold Owned_Int
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  simp [-UInt_bytes]
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
+  unfold Owned_UInt Owned
+  solve_owned_eq_unsigned UInt_bytes
+    points_agree_addr_1 points_agree_addr_i v v'
 
 theorem Owned_ULong_eq {l : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_ULong l v -∗ Owned_ULong l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_ULong
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  simp [-ULong_bytes]
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
+  unfold Owned_ULong Owned
+  solve_owned_eq_unsigned ULong_bytes
+    points_agree_addr_1 points_agree_addr_i v v'
+
+theorem Owned_Char_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Char l v -∗ Owned_Char l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_Char Owned
+  iintro ⟨%x , ⟨%HEq , ⟨HOwn , ⟨%Hb , _⟩⟩⟩⟩ ⟨%x' , ⟨%HEq' , ⟨HOwn' , ⟨%Hb' , _⟩⟩⟩⟩
+  simp
+  icases HOwn with ⟨H1 , _⟩; icases HOwn' with ⟨H2 , _⟩
+  have HEq : x' = x := by simpa [HEq'] using HEq
+  rw [HEq]
+  ihave %H := pointsTo_agree $$ [$]
+  injection H with val_eq; ipureintro
+  exact (wrapI_Char_cong Hb Hb' (val_eq.symm))
+
+theorem Owned_Short_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Short l v -∗ Owned_Short l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_Short Owned
+  solve_owned_eq_signed UShort_bytes points_agree_addr_1 points_agree_addr_i
+    min_UShort max_UShort v v'
+
+theorem Owned_Int_eq {l : Ptr} {v v' : Int} :
+  ⊢@{IProp GF} Owned_Int l v -∗ Owned_Int l v' -∗ ⌜v = v'⌝ :=
+by
+  unfold Owned_Int Owned
+  solve_owned_eq_signed UInt_bytes points_agree_addr_1 points_agree_addr_i
+    min_UInt max_UInt v v'
 
 theorem Owned_Long_eq {l : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_Long l v -∗ Owned_Long l v' -∗ ⌜v = v'⌝ :=
 by
-  unfold Owned_Long
-  iintro ⟨HOwn, %H⟩ ⟨HOwn', %H'⟩
-  simp [-ULong_bytes]
-  solve_owned_eq Owned_UChar_eq (HOwn, HOwn') v v' H H'
+  unfold Owned_Long Owned
+  solve_owned_eq_signed ULong_bytes points_agree_addr_1 points_agree_addr_i
+    min_ULong max_ULong v v'
 
--- 6, Pointer inequality theorems for Owned and Block
+-- 5, Pointer inequality theorems for Owned and Block
 
 theorem Owned_UChar_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_UChar l v -∗ Owned_UChar l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Owned_UChar
-  iintro ⟨H1 , _⟩ ⟨H2 , _⟩
-  iapply pointsTo_ne $$ H1 H2
+  solve_own_neq Owned_UChar Owned
 
 theorem Owned_Char_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_Char l v -∗ Owned_Char l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Owned_Char
-  iintro ⟨H1 , _⟩ ⟨H2 , _⟩
-  iapply Owned_UChar_neq $$ H1 H2
+  solve_own_neq Owned_Char Owned
 
 theorem Owned_UShort_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_UShort l v -∗ Owned_UShort l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_UShort Owned_UChar_neq
+  solve_own_neq Owned_UShort Owned
 
 theorem Owned_Short_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_Short l v -∗ Owned_Short l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_Short Owned_UChar_neq
+  solve_own_neq Owned_Short Owned
 
 theorem Owned_UInt_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_UInt l v -∗ Owned_UInt l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_UInt Owned_UChar_neq
+  solve_own_neq Owned_UInt Owned
 
 theorem Owned_Int_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_Int l v -∗ Owned_Int l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_Int Owned_UChar_neq
+  solve_own_neq Owned_Int Owned
 
 theorem Owned_ULong_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_ULong l v -∗ Owned_ULong l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_ULong Owned_UChar_neq
+  solve_own_neq Owned_ULong Owned
 
 theorem Owned_Long_neq {l l' : Ptr} {v v' : Int} :
   ⊢@{IProp GF} Owned_Long l v -∗ Owned_Long l' v' -∗ ⌜l ≠ l'⌝ :=
 by
-  solve_own_neq Owned_Long Owned_UChar_neq
+  solve_own_neq Owned_Long Owned
 
 theorem Block_Char_neq {l l' : Ptr} :
   ⊢@{IProp GF} Block_Char l -∗ Block_Char l' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Block_Char
-  iintro ⟨H , _⟩ ⟨H' , _⟩
-  iapply pointsTo_ne $$ H H'
+  solve_own_neq Block_Char Block
 
 theorem Block_Short_neq {l l' : Ptr} :
   ⊢@{IProp GF} Block_Short l -∗ Block_Short l' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Block_Short
-  iintro ⟨H , _⟩ ⟨H' , _⟩; iapply Block_Char_neq $$ H H'
+  solve_own_neq Block_Short Block
 
 theorem Block_Int_neq {l l' : Ptr} :
   ⊢@{IProp GF} Block_Int l -∗ Block_Int l' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Block_Int; simp
-  iintro ⟨H , _⟩ ⟨H' , _⟩; iapply Block_Char_neq $$ H H'
+  solve_own_neq Block_Int Block
 
 theorem Block_Long_neq {l l' : Ptr} :
   ⊢@{IProp GF} Block_Long l -∗ Block_Long l' -∗ ⌜l ≠ l'⌝ :=
 by
-  unfold Block_Long; simp
-  iintro ⟨H , _⟩ ⟨H' , _⟩; iapply Block_Char_neq $$ H H'
+  solve_own_neq Block_Long Block
--- CN_Lib/CN_Lib_Tactics.lean
+++ CN_Lib/CN_Lib_Tactics.lean
@@ -1,5 +1,6 @@
 import Iris.Proofmode.Tactics
 import Iris.BI.Lib.Fixpoint
+import Iris.BI.Lib.GenHeap
 import Qq
 import Batteries.Tactic.SeqFocus
 import Lean.Elab
@@ -8,9 +9,9 @@ import CN_Lib.CN_Lib
 
 open Lean Elab Tactic Qq
 
--- This file contains some useful tactics for proofs involving CN ownership predicates over integer types.
-
 namespace CN_Lib
+
+-- This file contains some useful tactics for proofs involving CN ownership predicates over integer types.
 
 declare_syntax_cat ident_tuple
 scoped syntax "(" ident "," ident ")":ident_tuple
@@ -27,22 +28,25 @@ macro_rules
         own_eq $f $[$tl]*)
 
 -- Tactic that destructs owned predicates into their component Owned_UChar predicates
-scoped syntax "destruct_owned" pmTerm_tuple ident : tactic
+scoped syntax "destruct_owned" pmTerm_tuple ident ident : tactic
 macro_rules
-  | `(tactic| destruct_owned ($HOwn, $HOwn') $f) =>
+  | `(tactic| destruct_owned ($HOwn, $HOwn') $f1 $fi) =>
       `(tactic| first
         | icases $HOwn with ⟨H1, ⟨H2, ⟨H3, ⟨H4, HOwn⟩⟩⟩⟩ <;>
           icases HOwn with ⟨H5, ⟨H6, ⟨H7, ⟨H8, _⟩⟩⟩⟩ <;>
           icases $HOwn' with ⟨H1', ⟨H2', ⟨H3', ⟨H4', HOwn'⟩⟩⟩⟩ <;>
           icases HOwn' with ⟨H5', ⟨H6', ⟨H7', ⟨H8', _⟩⟩⟩⟩ <;>
-          own_eq $f (H1, H1') (H2, H2') (H3, H3') (H4, H4')
+          own_eq $f1 (H1, H1')
+          own_eq $fi (H2, H2') (H3, H3') (H4, H4')
             (H5, H5') (H6, H6') (H7, H7') (H8, H8')
         | icases $HOwn with ⟨H1, ⟨H2, ⟨H3, ⟨H4, _⟩⟩⟩⟩ <;>
           icases $HOwn' with ⟨H1', ⟨H2', ⟨H3', ⟨H4', _⟩⟩⟩⟩ <;>
-          own_eq $f (H1, H1') (H2, H2') (H3, H3') (H4, H4')
-        | icases $HOwn with ⟨H1, H2⟩ <;>
-          icases $HOwn' with ⟨H1', H2'⟩ <;>
-          own_eq $f (H1, H1') (H2, H2')
+          own_eq $f1 (H1, H1')
+          own_eq $fi (H2, H2') (H3, H3') (H4, H4')
+        | icases $HOwn with ⟨H1, ⟨H2 , _⟩⟩ <;>
+          icases $HOwn' with ⟨H1', ⟨H2' , _⟩⟩ <;>
+          own_eq $f1 (H1, H1')
+          own_eq $fi (H2, H2')
           )
 
 -- Macro that proves `f v = f v'` from `f : Fin n → Int` and seperate hypotheses stating
@@ -55,58 +59,51 @@ macro_rules
       apply Fin.elim0 <;> assumption)
 
 -- Tactic that solves goals of the form ⌜v = v'⌝ given a pair of owned signed predicates
-elab "solve_owned_eq_signed" HOwn:pmTerm_tuple f:ident
-  f_bytes:ident min:ident max:ident
-  v:ident v':ident H:ident H':ident : tactic => do
+elab "solve_owned_eq_signed"
+  f_bytes:ident f1:ident fi:ident
+  min:ident max:ident
+  v:ident v':ident : tactic => do
   evalTactic <| ← `(tactic|
-    destruct_owned $HOwn $f <;> ipureintro <;>
+    (iintro ⟨%x , ⟨%HEq , ⟨HOwn , ⟨%H , _⟩⟩⟩⟩ ⟨%x' , ⟨%HEq' , ⟨HOwn' , ⟨%H' , _⟩⟩⟩⟩) <;>
+    simp [-$f_bytes] <;>
+    (have HEq : x' = x := by simpa [HEq'] using HEq) <;> rw [HEq] <;>
+    destruct_owned (HOwn,HOwn') $f1 $fi <;> ipureintro <;>
     (have Hbytes : $f_bytes (wrapI $min $max $v) = $f_bytes (wrapI $min $max $v') := by
       solve_fin_from_cases) <;>
     have Hv := wrapI_within_bounds (x := $v) (min := $min) (max := $max) (by lia) <;>
     have Hv' := wrapI_within_bounds (x := $v') (min := $min) (max := $max) (by lia) <;>
     (first
-      | have HEq := CN_Lib.UShort_eq Hv Hv' Hbytes <;> apply wrapI_Short_cong $H $H' HEq
-      | have HEq := CN_Lib.UInt_eq Hv Hv' Hbytes <;> apply wrapI_Int_cong $H $H' HEq
-      | have HEq := CN_Lib.ULong_eq Hv Hv' Hbytes <;> apply wrapI_Long_cong $H $H' HEq)
+      | have HEq := CN_Lib.UShort_eq Hv Hv' Hbytes <;> apply wrapI_Short_cong H H' HEq
+      | have HEq := CN_Lib.UInt_eq Hv Hv' Hbytes <;> apply wrapI_Int_cong H H' HEq
+      | have HEq := CN_Lib.ULong_eq Hv Hv' Hbytes <;> apply wrapI_Long_cong H H' HEq)
     )
 
 -- Tactic that solves goals of the form ⌜v = v'⌝ given a pair of owned unsigned predicates
-elab "solve_owned_eq_unsigned" HOwn:pmTerm_tuple f:ident
-  f_bytes:ident
-  v:ident v':ident H:ident H':ident : tactic => do
+elab "solve_owned_eq_unsigned"
+  f_bytes:ident f1:ident fi:ident
+  v:ident v':ident: tactic => do
   evalTactic <| ← `(tactic|
-    destruct_owned $HOwn $f <;> ipureintro <;>
+    (iintro ⟨%x , ⟨%HEq , ⟨HOwn , ⟨%H , _⟩⟩⟩⟩ ⟨%x' , ⟨%HEq' , ⟨HOwn' , ⟨%H' , _⟩⟩⟩⟩) <;>
+    simp [-$f_bytes] <;>
+    (have HEq : x' = x := by simpa [HEq'] using HEq) <;> rw [HEq] <;>
+    destruct_owned (HOwn, HOwn') $f1 $fi <;> ipureintro <;>
     (have Hbytes : $f_bytes $v = $f_bytes $v' := by
       funext i
       repeat refine Fin.lastCases ?_ ?_ i; lia; intro i
       apply Fin.elim0; assumption) <;>
     (first
-      | have HEq := CN_Lib.ULong_eq $H $H' Hbytes <;> assumption
-      | have HEq := CN_Lib.UInt_eq $H $H' Hbytes <;> assumption
-      | have HEq := CN_Lib.UShort_eq $H $H' Hbytes <;> assumption
-      )
-    )
-
--- Tactic that tries to apply the above two tactics for each supported C integer type
-elab "solve_owned_eq" f:ident HOwn:pmTerm_tuple
-  v:ident v':ident
-  H:ident H':ident : tactic => do
-  evalTactic <| ← `(tactic|
-    (first
-      | solve_owned_eq_unsigned $HOwn $f ULong_bytes $v $v' $H $H'
-      | solve_owned_eq_unsigned $HOwn $f UInt_bytes $v $v' $H $H'
-      | solve_owned_eq_unsigned $HOwn $f UShort_bytes $v $v' $H $H'
-      | solve_owned_eq_signed $HOwn $f UInt_bytes min_UInt max_UInt $v $v' $H $H'
-      | solve_owned_eq_signed $HOwn $f UShort_bytes min_UShort max_UShort $v $v' $H $H'
-      | solve_owned_eq_signed $HOwn $f ULong_bytes min_ULong max_ULong $v $v' $H $H'
+      | have HEq := CN_Lib.ULong_eq H H' Hbytes <;> assumption
+      | have HEq := CN_Lib.UInt_eq H H' Hbytes <;> assumption
+      | have HEq := CN_Lib.UShort_eq H H' Hbytes <;> assumption
       )
     )
 
 -- Tactic that solves inequalities of the form ⌜l ≠ l'⌝ given a pair of owned predicates
 scoped syntax "solve_own_neq" ident ident: tactic
 macro_rules
-  | `(tactic| solve_own_neq $f $g) =>
-      `(tactic| unfold $f <;>
-          iintro ⟨H1 , _⟩ ⟨H2 , _⟩; simp <;>
-          icases H1 with ⟨H1 , _⟩; icases H2 with ⟨H2 , _⟩ <;>
-          iapply $g:ident $$ H1 H2)
+  | `(tactic| solve_own_neq $f1 $f2) =>
+      `(tactic| unfold $f1 $f2 <;>
+          iintro ⟨%x , ⟨%HEq , ⟨HOwn , _⟩⟩⟩ ⟨%x' , ⟨%HEq' , ⟨HOwn' , _⟩⟩⟩ <;>
+          simp <;>
+          icases HOwn with ⟨H1 , _⟩; icases HOwn' with ⟨H2 , _⟩ <;>
+          (ihave %HNeq := Iris.pointsTo_ne $$ H1 H2; ipureintro) <;> lia)
